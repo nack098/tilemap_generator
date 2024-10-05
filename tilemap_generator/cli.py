2cfb976a629392a58ae0f16e02605154876c6bbe
nack098 <nack098gamer@gmail.com>
2024-10-05 22:11:58 +0700

    feat(cli): split code

--- tilemap_generator/cli.py
+++ tilemap_generator/cli.py
@@ -15,19 +15,27 @@ def log(message: str, endline: bool = False):
     print(f"{now_str} [\u001b[32mapplication\u001b[0m] {message}", end="\r")
 
 
-def start(path: Path):
-    image: np.ndarray = mimg.imread(path)
+def crop(image: np.ndarray, tile_size: tuple) -> np.ndarray:
     im_h, im_w, ch = image.shape
-    tile_h, tile_w = (16, 16)
-    log("Cropping Image")
+    tile_h, tile_w = tile_size[0], tile_size[1]
     tiled = image.reshape(im_h // tile_h, tile_h, im_w // tile_w, tile_w, ch)
     tiled = tiled.swapaxes(1, 2)
-    new = tiled.reshape(-1, 16, 16, 3)
+    return tiled.reshape(-1, 16, 16, 3)
+
+
+def generate(outDir: Path, tiles: np.ndarray):
+    for i, tile in enumerate(tiles):
+        mimg.imsave(outDir.joinpath(f"{i}.bmp"), tile)
+
+
+def start(path: Path):
+    image: np.ndarray = mimg.imread(path)
+    log("Cropping Image")
+    tiles = crop(image, (16, 16))
     log("Generating Tilemap")
     out = Path(getcwd()).joinpath("out")
     out.mkdir(parents=True, exist_ok=True)
-    for i, tile in enumerate(new):
-        mimg.imsave(out.joinpath(f"{i}.bmp"), tile)
+    generate(out, tiles)
 
 
 def main():
