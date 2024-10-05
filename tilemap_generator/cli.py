82d27045c663e31f4c7ae5bc1d60ae80ada4059f
nack098 <nack098gamer@gmail.com>
2024-10-05 22:05:32 +0700

    feat(cli): add log

--- tilemap_generator/cli.py
+++ tilemap_generator/cli.py
@@ -1,19 +1,29 @@
 import matplotlib.image as mimg
 import numpy as np
+from datetime import datetime
 from os import getcwd
 from sys import argv as args
 from pathlib import Path
+
+
+def log(message: str, endline: bool = False):
+    now: datetime = datetime.now()
+    now_str: str = now.strftime("%Y-%m-%d %H:%M:%S")
+    if not endline:
+        print(f"{now_str} [\u001b[32mapplication\u001b[0m] {message}")
+        return
+    print(f"{now_str} [\u001b[32mapplication\u001b[0m] {message}", end="\r")
 
 
 def start(path: Path):
     image: np.ndarray = mimg.imread(path)
     im_h, im_w, ch = image.shape
     tile_h, tile_w = (16, 16)
-    print("[\u001b[32mapplication\u001b[0m] Cropping Image...")
+    log("Cropping Image")
     tiled = image.reshape(im_h // tile_h, tile_h, im_w // tile_w, tile_w, ch)
     tiled = tiled.swapaxes(1, 2)
     new = tiled.reshape(-1, 16, 16, 3)
-    print("[\u001b[32mapplication\u001b[0m] Generating Tilemap")
+    log("Generating Tilemap")
     out = Path(getcwd()).joinpath("out")
     out.mkdir(parents=True, exist_ok=True)
     for i, tile in enumerate(new):
